a949bf146448bbdd7b322e7db5cd887e0b475361
nick_baccete <nickbaccete@gmail.com>
2020-09-14 15:32:24 -0400

    First method done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,20 @@
 class Restaurant < ActiveRecord::Base
   has_many :dishes
+
+
+  def self.mcdonalds
+    Restaurant.find_by(name:"McDonalds")
+  end
+
+
+  def self.tenth
+    restaurants
+  end
+
+
+
+
+
+
+
 end
