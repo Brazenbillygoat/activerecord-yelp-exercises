32f2576e6469c7630feb8710f8ea4c3b4c190af0
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-14 15:15:51 -0400

    Seed file completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 require 'faker'
+require 'pry'
 
+Tag.destroy_all
+Restaurant.destroy_all
+Dish.destroy_all
+DishTag.destroy_all
 
-
-restaurants = ["Olive Garden", "Macdonalds", "Burger King", "Wendy's", "Subway", "Denny's", "Popeyes", "KFC", "Nick's Italian", "Woodfire Grill", "Macaroni Grill", "Cheesecake Factory", "Paul's", "IHOP", "Outback Steakhouse", "LongHorn Steakhouse", "Red Hot & Blue Laurels", "Buffalo Wild Wings", "Shake Shack", "Five Guys"]
+restaurants = ["Olive Garden", "McDonalds", "Burger King", "Wendy's", "Subway", "Denny's", "Popeyes", "KFC", "Nick's Italian", "Woodfire Grill", "Macaroni Grill", "Cheesecake Factory", "Paul's", "IHOP", "Outback Steakhouse", "LongHorn Steakhouse", "Red Hot & Blue Laurels", "Buffalo Wild Wings", "Shake Shack", "Five Guys"]
 
 dishs = ["Pizza", "Nachos", "Hotdog", "Boneless Wings", "Cheese Burger", "Fries", "Waffles", "Pancakes", "Chicken Sandwhich", "Ice Water"]
 
@@ -10,18 +14,48 @@ tags = ["Spicy", "Vegetarian", "Vegan", "Gluten-free", "Breakfast", "Lunch", "Di
 
 #create 20 restaurants with array .each
 
-#Restaurant.create(name: "#{Faker::Restaurant.name}")
 
-#and each one needs 10 dishs
+restaurants.each do |r|
+  Restaurant.create(name: r)
+end
 
-# Restaurant.all.each do |r|
-#   3.times(Dish.create(name: "Faker::Food.name", restaurant_id: r.id))
-# end
+#create 10 dishes and each one needs a restaurant id
+
+Restaurant.all.each do |r|
+  dish_count = 0
+  while dish_count < 10 do
+    Dish.create(name: Faker::Food.name, restaurant_id: r.id)
+    dish_count += 1
+  end
+end
 
 
+#creating 15 tags
+
+tags.each do |t|
+  Tag.create(name: t)
+end
+
+
+#Populate join table
 #each dish needs 3 tags
 
 
+
+Dish.all.each do |d|
+  tag_indices = Tag.all.map do |t|
+    t.id
+  end
+  tag_count = 0
+  
+  while tag_count < 3 do
+    samp = tag_indices.sample
+    DishTag.create(dish_id: d.id, tag_id: Tag.all.find(samp).id)
+    tag_indices.delete_at(samp)
+    tag_count += 1
+  end
+
+end
 
 
 
