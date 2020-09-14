3972a3c2fe84424daad7232205232265485baa8b
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-14 14:03:04 -0400

    added functionality to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'faker'
 
 
+
 restaurants = ["Olive Garden", "Macdonalds", "Burger King", "Wendy's", "Subway", "Denny's", "Popeyes", "KFC", "Nick's Italian", "Woodfire Grill", "Macaroni Grill", "Cheesecake Factory", "Paul's", "IHOP", "Outback Steakhouse", "LongHorn Steakhouse", "Red Hot & Blue Laurels", "Buffalo Wild Wings", "Shake Shack", "Five Guys"]
 
 dishs = ["Pizza", "Nachos", "Hotdog", "Boneless Wings", "Cheese Burger", "Fries", "Waffles", "Pancakes", "Chicken Sandwhich", "Ice Water"]
@@ -8,15 +9,21 @@ dishs = ["Pizza", "Nachos", "Hotdog", "Boneless Wings", "Cheese Burger", "Fries"
 tags = ["Spicy", "Vegetarian", "Vegan", "Gluten-free", "Breakfast", "Lunch", "Dinner", "Brunch", "Side", "Fat", "Delivery", "Healthy", "Hot", "Cold", "Raw"]
 
 #create 20 restaurants with array .each
-#Restaurant.create(name: "#{rest}")
+
+#Restaurant.create(name: "#{Faker::Restaurant.name}")
 
 #and each one needs 10 dishs
 
-#each dish needs 3 tags
-
-# restaurant.each do |rest|
-#   Restaurant.create(name: "#{rest}")
+# Restaurant.all.each do |r|
+#   3.times(Dish.create(name: "Faker::Food.name", restaurant_id: r.id))
 # end
 
-puts Faker::Name.name
-puts Faker::Restaurant.name
+
+#each dish needs 3 tags
+
+
+
+
+
+
+
